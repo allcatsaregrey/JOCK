79da429b37e20f8fc9133c8b1b1635aa764cd2c6
Matt <52715117+allcatsaregrey@users.noreply.github.com>
2020-04-30 20:24:37 -0400

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,8 @@
 
 # TODO: Each key of the ID dictionary should contain a dictionary
 # with keys such as start time, end time etc. so a full description
-# of the auction is available.
+# of the auction is available. We also need to add functions to
+# sort the auctions with in the database to certain specifications.
 
 
 class jock_data_base():
@@ -38,6 +39,7 @@ class jock_data_base():
             for keywd in keywds:
                 if keywd in desc.split():
                     id_matches.append(id)
+        return id_matches
 
     def reinit_(self):
 
