c9a7e1b2a378de73065d74e012e93858d3f9e3c6
Matt <52715117+allcatsaregrey@users.noreply.github.com>
2020-04-30 20:24:45 -0400

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,13 @@
+# Author: Matthew C. McFee
+# Description: This module provides scraping functionality for
+# Yahoo Japan.
+
+# TODO: Functions to specify certain tags on Yahoo Japan (ie. brands),
+# functions to scrape Yahoo auctions search results for specified tag,
+# functions to store newly found actions with information into a database,
+# functions that specify foreign IP accesses to an auctions page and counts
+# said accesses.
+
+if __name__ == "__main__":
+
+    pass
