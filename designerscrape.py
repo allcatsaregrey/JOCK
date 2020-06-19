623040cb82779fff1e9691ec725f594a61a13105
cvartanian <cvchris11@gmail.com>
2020-06-19 13:34:47 -0400

    Partial Edits and Changes

--- designerscrape.py
+++ designerscrape.py
@@ -2,12 +2,18 @@
 #Batch Scraper Decently Unstable
 import scrape
 import database
+import time
+import random
+
 
 database = database.jock_data_base()
 
- 
+start_time = time.time()
+
 f = open("designers.txt", "r")
 for SEARCH in f:  
+    
+    
     
     auctionmaxint, binmaxint = scrape.auction_search_(SEARCH)
     
@@ -18,3 +24,12 @@ for SEARCH in f:
     scrape.auction_bin_parse(database, functbmax, SEARCH)
     
     database.write_csv()
+    
+    elapsed_time = time.time() - start_time
+    
+    time.sleep(random.randint(10,100))
+    
+    print(elapsed_time)
+    
+    print("\n")
+    
